ffa354504a6f889a01a7cb5479baaba9d17bcb2c
medlemine-djeidjah <164183621+medlemine-djeidjah@users.noreply.github.com>
2024-05-10 12:41:31 -0800

    Added leaflet js map to home page with current loccation marker.

--- MecaniAide/gps/views.py
+++ MecaniAide/gps/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
-
+#@login_required(login_url= "auth/login/")
 def home(request):
     return render(request, 'home.html')
