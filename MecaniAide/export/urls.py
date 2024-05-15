1a599818fc50580420857850ece04c3c8cacc987
medlemine-djeidjah <164183621+medlemine-djeidjah@users.noreply.github.com>
2024-05-15 09:51:23 -0800

    Added Foriegn key from car parts to orders.

--- MecaniAide/export/urls.py
+++ MecaniAide/export/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import upload_csv
+from .views import upload_csv, export_csv
 
 urlpatterns = [
     path('upload-csv/', upload_csv, name='upload_csv'),
+    path('export-csv/', export_csv, name='export_csv'),
 ]
--- MecaniAide/export/views.py
+++ MecaniAide/export/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.contrib import messages
-from store.models import CarPart
+from store.models import CarPart, Order
 import csv
-from io import TextIOWrapper
 
 
 def upload_csv(request):
@@ -16,17 +16,34 @@ def upload_csv(request):
         else:
             handle_uploaded_csv(csv_file)
             messages.success(request, 'CSV file uploaded successfully.')
+            return redirect('car_parts_store')  # Redirect to view orders after successful upload
     return render(request, 'upload_csv.html')
-
-
 
 def handle_uploaded_csv(csv_file):
     decoded_file = csv_file.read().decode('utf-8').splitlines()
     reader = csv.DictReader(decoded_file)
     for row in reader:
+        order_number = row.get('order_number')  # Assuming 'order_number' column exists in CSV
+        order, _ = Order.objects.get_or_create(order_number=order_number)
         CarPart.objects.create(
             name=row['name'],
             description=row['description'],
             price=row['price'],
-            image_url=row['image_url']
+            image_url=row['image_url'],
+            order=order
         )
+
+def export_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="orders.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['Order Number', 'Date Ordered', 'Name', 'Description', 'Price'])
+
+    orders = Order.objects.all()
+    for order in orders:
+        car_parts = order.car_parts.all()
+        for part in car_parts:
+            writer.writerow([order.order_number, order.date_ordered, part.name, part.description, part.price])
+
+    return response
