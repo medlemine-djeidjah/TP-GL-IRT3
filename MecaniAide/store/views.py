4e22b34a218b719d3c46cc8f07151aafebc1d30a
Ethmanemahfoudh <23059@esp.mr>
2024-05-15 11:11:08 +0000

    voici les taches : 1. page profile 2. side bar 3. store

--- MecaniAide/store/views.py
+++ MecaniAide/store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import CarPart
+
+def car_parts_store(request):
+    parts = CarPart.objects.all()
+    return render(request, 'car_parts_store.html', {'parts': parts})
