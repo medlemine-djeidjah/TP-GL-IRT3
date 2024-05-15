64f8bb12a81a57bc4b3b2d0bd790020c1c2cdf52
med23009 <23009@esp.mr>
2024-05-15 17:05:06 +0000

    correction

--- MecaniAide/export/models.py
+++ MecaniAide/export/models.py
@@ -1,14 +1,3 @@
 from django.db import models
 
-
-class CarPart(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    image_url = models.URLField()
-
-    def __str__(self):
-        return self.name
-
-
 # Create your models here.
