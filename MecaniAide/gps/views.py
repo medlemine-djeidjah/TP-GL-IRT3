96d59153a26cdda22b652181bd19c99ada6b2775
medlemine-djeidjah <164183621+medlemine-djeidjah@users.noreply.github.com>
2024-05-25 15:46:31 -0800

    Fixed login / registre pages styles

--- MecaniAide/gps/views.py
+++ MecaniAide/gps/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
-#@login_required(login_url= "auth/login/")
+@login_required(login_url= "auth/login/")
 def home(request):
     return render(request, 'home.html', {'user': request.user})
 
